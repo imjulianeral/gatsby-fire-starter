72df7e76d41734391557e5066b9e7aa46054ab57
Jorge <julianacero98@gmail.com>
2020-05-27 00:57:50 -0500

    FirebaseUI bug fixed

--- src/components/FirebaseUI.jsx
+++ src/components/FirebaseUI.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, { memo } from 'react'
 import FirebaseUIAuth from 'react-firebaseui-localized'
 import { useAuth, useFirebaseApp } from 'reactfire'
 
-export default function FirebaseUI() {
+const FirebaseUI = memo(() => {
   const { firebase_ } = useFirebaseApp()
   const auth = useAuth
-
   return (
-    <>
-      <FirebaseUIAuth
-        lang="es_419"
-        config={{
-          // Popup signin flow rather than redirect flow.
-          signInFlow: 'popup',
-          // We will display Google and Email as auth providers.
-          signInOptions: [
-            auth.GoogleAuthProvider.PROVIDER_ID,
-            auth.EmailAuthProvider.PROVIDER_ID,
-          ],
-          signInSuccessUrl: '/app/page-2',
-        }}
-        auth={auth()}
-        firebase={firebase_}
-      />
-    </>
+    <FirebaseUIAuth
+      lang="es_419"
+      config={{
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // We will display Google and Email as auth providers.
+        signInOptions: [
+          auth.GoogleAuthProvider.PROVIDER_ID,
+          auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        signInSuccessUrl: '/app/page-2',
+      }}
+      auth={auth()}
+      firebase={firebase_}
+    />
   )
-}
+})
+
+export default FirebaseUI
